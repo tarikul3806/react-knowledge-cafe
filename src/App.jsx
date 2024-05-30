c781d78b28f7f68031ba0a88042d154d1bc50cf7
tarikul3806 <mdselim156379@gmail.com>
2024-05-30 13:15:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </>
+  )
+}
+
+export default App
